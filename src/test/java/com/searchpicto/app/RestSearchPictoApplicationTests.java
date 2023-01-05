87ac1aefba1e285828873cc5c147149a70b1a93a
Caroline Aupert <caroline.aupert@gmail.com>
2023-01-05 15:13:40 +0100

    Base project added

---
+++ src/test/java/com/searchpicto/app/RestSearchPictoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchpicto.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSearchPictoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
