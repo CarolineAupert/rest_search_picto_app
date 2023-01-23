649e7c164dbc2ecacbce83d6c5543fa90f8613a8
Caroline Aupert <caroline.aupert@gmail.com>
2023-01-23 18:08:54 +0100

    Avancement web service recup picto

--- src/main/java/com/searchpicto/ws/model/Media.java
+++ src/main/java/com/searchpicto/ws/model/Media.java
@@ -1,7 +1,95 @@
 package com.searchpicto.ws.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="MEDIAS")
 public class Media {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long mediaId;
 	
+	private float weight;
+	
+	@Column(nullable=false)
+	private String location;
+	
+	@OneToOne(mappedBy = "media")
+	private Picto picto;
+	
+	// Todo mettre enum avec ce qui est accepté
+	@Column(nullable=false)
+	private String type;
+	/**
+	 * mediaId getter.
+	 *
+	 * @return the mediaId.
+	 */
+	public Long getMediaId() {
+		return mediaId;
+	}
+	/**
+	 * mediaId setter.
+	 *
+	 * @param mediaId : the mediaId to set.
+	 */
+	public void setMediaId(Long mediaId) {
+		this.mediaId = mediaId;
+	}
+	/**
+	 * weight getter.
+	 *
+	 * @return the weight.
+	 */
+	public float getWeight() {
+		return weight;
+	}
+	/**
+	 * weight setter.
+	 *
+	 * @param weight : the weight to set.
+	 */
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	/**
+	 * location getter.
+	 *
+	 * @return the location.
+	 */
+	public String getLocation() {
+		return location;
+	}
+	/**
+	 * location setter.
+	 *
+	 * @param location : the location to set.
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	/**
+	 * type getter.
+	 *
+	 * @return the type.
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * type setter.
+	 *
+	 * @param type : the type to set.
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
 
 }
--- src/main/java/com/searchpicto/ws/model/Picto.java
+++ src/main/java/com/searchpicto/ws/model/Picto.java
@@ -3,8 +3,84 @@
  */
 package com.searchpicto.ws.model;
 
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="PICTOS")
 public class Picto {
-	
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long pictoId;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "media_id", referencedColumnName = "mediaId")
+	private Media media;
+	
+	@ManyToMany
+	@JoinTable(name = "PICTOS_TAGS", 
+			  joinColumns = @JoinColumn(name = "picto_id"), 
+			  inverseJoinColumns = @JoinColumn(name = "tag_id"))
+	private Set<Tag> tags;
+	
+	/**
+	 * pictoId getter.
+	 *
+	 * @return the pictoId.
+	 */
+	public Long getPictoId() {
+		return pictoId;
+	}
+	/**
+	 * pictoId setter.
+	 *
+	 * @param pictoId : the pictoId to set.
+	 */
+	public void setPictoId(Long pictoId) {
+		this.pictoId = pictoId;
+	}
+	/**
+	 * media getter.
+	 *
+	 * @return the media.
+	 */
+	public Media getMedia() {
+		return media;
+	}
+	/**
+	 * media setter.
+	 *
+	 * @param media : the media to set.
+	 */
+	public void setMedia(Media media) {
+		this.media = media;
+	}
+	/**
+	 * tags getter.
+	 *
+	 * @return the tags.
+	 */
+	public Set<Tag> getTags() {
+		return tags;
+	}
+	/**
+	 * tags setter.
+	 *
+	 * @param tags : the tags to set.
+	 */
+	public void setTags(Set<Tag> tags) {
+		this.tags = tags;
+	}
+	
 }
