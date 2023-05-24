6eaf04bdd463ffb2b9b6c3dc0b0f407a75c4fdf8
Caroline Aupert <caroline.aupert@gmail.com>
2023-05-24 15:40:20 +0200

    Update database policy : nothind is done on prod db on initialization

--- src/main/java/com/searchpicto/ws/RestSearchPictoApplication.java
+++ src/main/java/com/searchpicto/ws/RestSearchPictoApplication.java
@@ -1,22 +1,7 @@
 package com.searchpicto.ws;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
-import com.searchpicto.ws.model.Media;
-import com.searchpicto.ws.model.Picto;
-import com.searchpicto.ws.model.Tag;
-import com.searchpicto.ws.service.PictoService;
 
 @SpringBootApplication
 public class RestSearchPictoApplication {
@@ -24,38 +9,4 @@ public class RestSearchPictoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestSearchPictoApplication.class, args);
 	}
-
-	@Autowired
-	private Environment environment;
-
-	
-	// Will be removed
-	@Bean
-	CommandLineRunner demo(PictoService pictoService) {
-		return args -> {
-			if (!Arrays.asList(environment.getActiveProfiles()).contains("test")) {
-				pictoService.addNewPicto(initPicto("https://caukaro.fr/wp-content/uploads/2023/02/Loupe.jpg","Une loupe",
-						Stream.of("loupe", "détail", "chercher", "analyser", "zoom", "picto").collect(Collectors.toSet())));
-				pictoService.addNewPicto(initPicto("https://caukaro.fr/wp-content/uploads/2023/02/Parchemin.jpg","Un parchemin",
-						Stream.of("parchemin", "détail", "contrat", "législation", "picto").collect(Collectors.toSet())));
-				pictoService.addNewPicto(initPicto("https://caukaro.fr/wp-content/uploads/2023/02/Travail-ordi.jpg","Un perso avec son ordi",
-						Stream.of("ordinateur", "travail", "télétravail", "heureux", "picto", "perso").collect(Collectors.toSet())));
-				pictoService.addNewPicto(initPicto("https://caukaro.fr/wp-content/uploads/2023/02/Tirelire.jpg","Une tirelire cochon",
-						Stream.of("tirelire", "cochon", "argent", "picto").collect(Collectors.toSet())));
-				pictoService.addNewPicto(initPicto("https://caukaro.fr/wp-content/uploads/2023/02/Surf-plante.jpg","Une feuille pour planche de surf",
-						Stream.of("surf", "écologie", "plante", "picto", "perso").collect(Collectors.toSet())));
-				pictoService.addNewPicto(initPicto("https://caukaro.fr/wp-content/uploads/2023/02/Questionnement.jpg","Une question",
-						Stream.of("question", "perso", "picto", "réfléchir").collect(Collectors.toSet())));
-			}
-		};
-	}
-
-	private Picto initPicto(String location, String title, Set<String> tags) {
-		Picto picto = new Picto();
-		picto.setCreationDate(LocalDateTime.now());
-		picto.setMedia(new Media(location, title));
-		picto.setTags(tags.stream().map(Tag::new).collect(Collectors.toSet()));
-		return picto;
-	}
-
 }
